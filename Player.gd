91a01e60307fa34f2b862b0759a994473017669e
Ben Gubler <nebrelbug@gmail.com>
2019-10-11 18:26:33 -0600

    weoighwe

--- Player.gd
+++ Player.gd
@@ -4,6 +4,12 @@ onready var camXY = get_node("Cam_XY")
 onready var camXZ = get_node("Cam_XZ")
 var currentCam : int = 1
 
+var x = 1 # these are bools
+var y = 1
+var z = 0
+
+
+
 # 1 is cam XY
 # 2 is cam XZ
 # 3 is cam YZ
@@ -27,12 +33,24 @@ func _input(event):
 			2:
 				camXZ.make_current()
 				print("dimension XZ")
+				x = 1
+				z = 1
+				y = 0
 			3:
 				camYZ.make_current()
 				print("dimension YZ")
+				x = 0
+				y = 1
+				z = 1
 			4:
 				currentCam = 1
 				camXY.make_current()
 				print("dimension XY")
+				x = 1
+				y = 1
+				z = 0
+				
+	if event.is_action_pressed("Down"):
+		move_and_collide(Vector3(1,0,0))
 
 
